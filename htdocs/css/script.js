3492ba306faf73b2539de07df26716f00c207668
WanderingMonument <Dingospoodle1@gmail.com>
2024-09-30 20:11:01 -0700

    commit

--- htdocs/css/script.js
+++ htdocs/css/script.js
@@ -2,5 +2,5 @@ var currentUrl = window.location.href;
 
 if (currentUrl.includes('?i=1')) {
 
-    window.location.href = "https://wanderingmonument.rf.gd/";
+    window.location.href = "https://wanderingmonument.rf.gd/css";
 }
